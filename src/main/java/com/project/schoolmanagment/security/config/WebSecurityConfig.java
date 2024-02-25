64278c83fbd87830c1cdabe28371bc8ec1fa71f8
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2024-02-25 12:43:26 +0100

    AuthTokenFilter bereit

---
+++ src/main/java/com/project/schoolmanagment/security/config/WebSecurityConfig.java
@@ -0,0 +1,4 @@
+package com.project.schoolmanagment.security.config;
+
+public class WebSecurityConfig {
+}
---
+++ src/main/java/com/project/schoolmanagment/security/jwt/AuthEntryPointJwt.java
@@ -0,0 +1,4 @@
+package com.project.schoolmanagment.security.jwt;
+
+public class AuthEntryPointJwt {
+}
