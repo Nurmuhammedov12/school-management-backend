f07e167cf4745b500be2cb9ec92256d3e242918e
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2024-02-27 00:53:29 +0100

    EducationTerm Save All method

--- src/main/java/com/project/schoolmanagment/controller/buisnes/EducationTermController.java
+++ src/main/java/com/project/schoolmanagment/controller/buisnes/EducationTermController.java
@@ -1,9 +1,17 @@
 package com.project.schoolmanagment.controller.buisnes;
 
+import com.project.schoolmanagment.payload.request.buisnes.EducationTermRequest;
+import com.project.schoolmanagment.payload.response.businnes.EducationTermResponse;
+import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
 import com.project.schoolmanagment.service.buisnes.EducationTermService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/educationTerms")
@@ -11,4 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class EducationTermController {
 
     private final EducationTermService educationTermService;
+
+    @PreAuthorize("hasAnyAuthority('Admin', 'Dean')")
+    @PostMapping("/save")
+    public ResponseMessage<EducationTermResponse> saveEducationTerm(@RequestBody @Valid
+                                                                    EducationTermRequest educationTermRequest){
+        return educationTermService.saveEducationTerm(educationTermRequest);
+    }
 }
--- src/main/java/com/project/schoolmanagment/repository/buisnes/EducationTermRepository.java
+++ src/main/java/com/project/schoolmanagment/repository/buisnes/EducationTermRepository.java
@@ -1,9 +1,21 @@
 package com.project.schoolmanagment.repository.buisnes;
 
 import com.project.schoolmanagment.entity.concretes.buisnes.EducationTerm;
+import com.project.schoolmanagment.entity.enums.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EducationTermRepository extends JpaRepository<EducationTerm, Long> {
+
+    @Query("SELECT (count (e) > 0) FROM EducationTerm e WHERE e.term=?1 and extract(YEAR FROM e.startDate) = ?2 ")
+    boolean existByTermAndYear(Term term, int year);
+
+    //find all education terms in a year
+    @Query("SELECT e FROM EducationTerm e WHERE extract(YEAR FROM e.startDate) = ?1 ")
+    List<EducationTerm> findByYear(int year);
+
 }
