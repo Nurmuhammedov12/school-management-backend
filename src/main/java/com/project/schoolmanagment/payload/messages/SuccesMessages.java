4012172401f65e73613807ee452b7c40e644c80c
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2024-02-26 22:31:17 +0100

    Education Term messages

--- src/main/java/com/project/schoolmanagment/payload/messages/SuccesMessages.java
+++ src/main/java/com/project/schoolmanagment/payload/messages/SuccesMessages.java
@@ -12,4 +12,10 @@ public class SuccesMessages {
     public static final String USER_UPDATE_MESSAGE = "User is Updated Successfully";
 
     public static final String USER_UPDATE_PASSWORD = "User Password Updated Successfully";
+
+
+    //Education term
+    public static final String EDUCATION_TERM_SAVE = "Education Term is Saved";
+    public static final String EDUCATION_TERM_UPDATE = "Education Term is Updated Successfully";
+    public static final String EDUCATION_TERM_DELETE = "Education Term is Deleted Successfully";
 }
