4e76ecdda2e4abb49a96dca71efa377ca2262a48
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2024-02-27 05:23:29 +0100

    Save Lesson method

--- src/main/java/com/project/schoolmanagment/service/buisnes/LessonService.java
+++ src/main/java/com/project/schoolmanagment/service/buisnes/LessonService.java
@@ -1,10 +1,16 @@
 package com.project.schoolmanagment.service.buisnes;
 
+import com.project.schoolmanagment.entity.concretes.buisnes.Lesson;
+import com.project.schoolmanagment.exception.NotFoundExceptions;
+import com.project.schoolmanagment.payload.mappers.LessonMapper;
+import com.project.schoolmanagment.payload.messages.ErrorMessages;
+import com.project.schoolmanagment.payload.messages.SuccesMessages;
 import com.project.schoolmanagment.payload.request.buisnes.LessonRequest;
 import com.project.schoolmanagment.payload.response.businnes.LessonResponse;
 import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
 import com.project.schoolmanagment.repository.buisnes.LessonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +18,24 @@ import org.springframework.stereotype.Service;
 public class LessonService {
 
     private final LessonRepository lessonRepository;
-    private final Les
+    private final LessonMapper lessonMapper;
 
     public ResponseMessage<LessonResponse> saveLesson(LessonRequest lessonRequest) {
+        //lessons must be unique
+        isLessonUnique(lessonRequest.getLessonName());
+        //map DTO -> entity
+        Lesson savedLesson = lessonMapper.mapLessonRequestToLesson(lessonRequest);
+        return ResponseMessage.<LessonResponse>builder()
+                .object(lessonMapper.mapLessonToLessonResponse(savedLesson))
+                .message(SuccesMessages.LESSON_SAVE)
+                .httpStatus(HttpStatus.CREATED)
+                .build();
 
+    }
+
+    private void isLessonUnique(String lessonName){
+        if (lessonRepository.getLessonNameEqualsIgnoreCase(lessonName).isPresent()){
+            throw new NotFoundExceptions(String.format(ErrorMessages.ALREADY_CREATED_LESSON_MESSAGE, lessonName));
+        }
     }
 }
