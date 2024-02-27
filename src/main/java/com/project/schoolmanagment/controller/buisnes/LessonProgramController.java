8bcaed313c7a6c1646a8e4f5c72234949376a33f
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2024-02-27 13:08:35 +0100

    Creating FindAll  method or function  Lesson Program

--- src/main/java/com/project/schoolmanagment/controller/buisnes/LessonProgramController.java
+++ src/main/java/com/project/schoolmanagment/controller/buisnes/LessonProgramController.java
@@ -6,12 +6,10 @@ import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
 import com.project.schoolmanagment.service.buisnes.LessonProgramService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/lessonProgram")
@@ -23,5 +21,11 @@ public class LessonProgramController {
     @PostMapping("/save")
     public ResponseMessage<LessonProgramResponse> saveLessonProgram(@RequestBody @Valid LessonProgramRequest lessonProgramRequest){
         return lessonProgramService.saveLessonProgram(lessonProgramRequest);
+    }
+
+    @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+    @GetMapping("/getAll")
+    public List<LessonProgramResponse> getAllLessonProgramByList(){
+        return lessonProgramService.getAllByList();
     }
 }
--- src/main/java/com/project/schoolmanagment/service/buisnes/LessonProgramService.java
+++ src/main/java/com/project/schoolmanagment/service/buisnes/LessonProgramService.java
@@ -1,21 +1,58 @@
 package com.project.schoolmanagment.service.buisnes;
 
+import com.project.schoolmanagment.entity.concretes.buisnes.EducationTerm;
+import com.project.schoolmanagment.entity.concretes.buisnes.Lesson;
+import com.project.schoolmanagment.entity.concretes.buisnes.LessonProgram;
+import com.project.schoolmanagment.payload.mappers.LessonProgramMapper;
+import com.project.schoolmanagment.payload.messages.SuccesMessages;
 import com.project.schoolmanagment.payload.request.buisnes.LessonProgramRequest;
 import com.project.schoolmanagment.payload.response.businnes.LessonProgramResponse;
 import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
 import com.project.schoolmanagment.repository.buisnes.LessonProgramRepository;
+import com.project.schoolmanagment.service.validator.DateTimeValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LessonProgramService {
 
     private final LessonProgramRepository lessonProgramRepository;
-    private final  EducationTermService educationTermService;
+    private final EducationTermService educationTermService;
     private final LessonsService lessonsService;
+    private final LessonProgramMapper lessonProgramMapper;
+    private final DateTimeValidator dateTimeValidator;
 
     public ResponseMessage<LessonProgramResponse> saveLessonProgram(LessonProgramRequest lessonProgramRequest) {
 
+        // LessonssService den ID ny teilen weil braucht fur DB
+        Set<Lesson> lessons = lessonsService.findLessonSetsById(lessonProgramRequest.getLessonIdList());
+
+        // Nimmt Aus education Term and suchst ID
+        EducationTerm educationTerm = educationTermService.isEducationTerm(lessonProgramRequest.getEducationTermId());
+
+        //Date Validation
+        dateTimeValidator.checkTimeWithException(lessonProgramRequest.getStartTime(), lessonProgramRequest.getStopTime());
+
+        LessonProgram lessonProgram = lessonProgramMapper.mapLessonProgramRequestToLessonProgram(lessonProgramRequest,lessons,educationTerm);
+        LessonProgram savedLessonProgram = lessonProgramRepository.save(lessonProgram);
+
+        return ResponseMessage.<LessonProgramResponse>builder()
+                .message(SuccesMessages.LESSON_PROGRAM_SAVE)
+                .httpStatus(HttpStatus.CREATED)
+                .object(lessonProgramMapper.mapLessonProgramToLessonProgramResponse(savedLessonProgram))
+                .build();
+    }
+
+    public List<LessonProgramResponse> getAllByList() {
+        return lessonProgramRepository.findAll()
+                .stream()
+                .map(lessonProgramMapper::mapLessonProgramToLessonProgramResponse)
+                .collect(Collectors.toList());
     }
 }
