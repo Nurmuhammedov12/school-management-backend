3219156c40813c3d4c5246a4536a2573f1ace8fa
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2024-02-27 05:07:48 +0100

    Lesson Lomboks

--- src/main/java/com/project/schoolmanagment/controller/buisnes/LessonController.java
+++ src/main/java/com/project/schoolmanagment/controller/buisnes/LessonController.java
@@ -1,4 +1,15 @@
 package com.project.schoolmanagment.controller.buisnes;
 
+import com.project.schoolmanagment.service.buisnes.LessonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lesson")
+@RequiredArgsConstructor
 public class LessonController {
+
+    private final LessonService lessonService;
+
 }
--- src/main/java/com/project/schoolmanagment/service/buisnes/LessonService.java
+++ src/main/java/com/project/schoolmanagment/service/buisnes/LessonService.java
@@ -1,4 +1,12 @@
 package com.project.schoolmanagment.service.buisnes;
 
+import com.project.schoolmanagment.repository.buisnes.LessonRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LessonService {
+
+    private final LessonRepository lessonRepository;
 }
