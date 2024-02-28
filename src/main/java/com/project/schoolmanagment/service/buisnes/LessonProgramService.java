f9fcc22ef2542c72ff73db303887987adb9193b8
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2024-02-28 19:54:38 +0100

    LessonProgram Pageable and delete

--- src/main/java/com/project/schoolmanagment/service/buisnes/LessonProgramService.java
+++ src/main/java/com/project/schoolmanagment/service/buisnes/LessonProgramService.java
@@ -3,14 +3,19 @@ package com.project.schoolmanagment.service.buisnes;
 import com.project.schoolmanagment.entity.concretes.buisnes.EducationTerm;
 import com.project.schoolmanagment.entity.concretes.buisnes.Lesson;
 import com.project.schoolmanagment.entity.concretes.buisnes.LessonProgram;
+import com.project.schoolmanagment.exception.NotFoundExceptions;
 import com.project.schoolmanagment.payload.mappers.LessonProgramMapper;
+import com.project.schoolmanagment.payload.messages.ErrorMessages;
 import com.project.schoolmanagment.payload.messages.SuccesMessages;
 import com.project.schoolmanagment.payload.request.buisnes.LessonProgramRequest;
 import com.project.schoolmanagment.payload.response.businnes.LessonProgramResponse;
 import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
 import com.project.schoolmanagment.repository.buisnes.LessonProgramRepository;
+import com.project.schoolmanagment.service.helper.PageableHelper;
 import com.project.schoolmanagment.service.validator.DateTimeValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +32,7 @@ public class LessonProgramService {
     private final LessonsService lessonsService;
     private final LessonProgramMapper lessonProgramMapper;
     private final DateTimeValidator dateTimeValidator;
+    private final PageableHelper pageableHelper;
 
     public ResponseMessage<LessonProgramResponse> saveLessonProgram(LessonProgramRequest lessonProgramRequest) {
 
@@ -54,5 +60,45 @@ public class LessonProgramService {
                 .stream()
                 .map(lessonProgramMapper::mapLessonProgramToLessonProgramResponse)
                 .collect(Collectors.toList());
+    }
+
+    public List<LessonProgramResponse> getAllUnassigned() {
+        return lessonProgramRepository.findByUsers_IdNull()
+                .stream()
+                .map(lessonProgramMapper::mapLessonProgramToLessonProgramResponse)
+                .collect(Collectors.toList());
+    }
+
+    public List<LessonProgramResponse> getAllAssigned() {
+        return lessonProgramRepository.findByUsers_IdNotNull()
+                .stream()
+                .map(lessonProgramMapper::mapLessonProgramToLessonProgramResponse)
+                .collect(Collectors.toList());
+    }
+
+
+    public LessonProgramResponse getLessonProgramById(Long id) {
+        return lessonProgramMapper.mapLessonProgramToLessonProgramResponse(idexistLessonProgram(id));
+    }
+
+    public LessonProgram idexistLessonProgram(Long id){
+        return lessonProgramRepository.findById(id).orElseThrow(
+                ()-> new NotFoundExceptions(ErrorMessages.NOT_FOUND_LESSON_PROGRAM_MESSAGE)
+        );
+    }
+
+    public ResponseMessage deleteById(Long id) {
+        idexistLessonProgram(id);
+        lessonProgramRepository.deleteById(id);
+        return ResponseMessage.builder()
+                .message(SuccesMessages.LESSON_PROGRAM_DELETE)
+                .build();
+    }
+
+    public Page<LessonProgramResponse> findLessonProgramByPage(int page, int size, String sort, String type) {
+        Pageable pageable = pageableHelper.getPageableWithProperties(page, size, sort, type);
+        return lessonProgramRepository
+                .findAll(pageable)
+                .map(lessonProgramMapper::mapLessonProgramToLessonProgramResponse);
     }
 }
