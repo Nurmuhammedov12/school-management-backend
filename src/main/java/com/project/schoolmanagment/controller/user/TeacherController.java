baa1aab20bee081ee2ced7f2ec88288bde200df8
Nurmuhammedov12 <nurmuhammedowyhlas8@gmail.com>
2024-02-29 11:58:53 +0100

    props

--- src/main/java/com/project/schoolmanagment/controller/user/TeacherController.java
+++ src/main/java/com/project/schoolmanagment/controller/user/TeacherController.java
@@ -1,22 +1,13 @@
 package com.project.schoolmanagment.controller.user;
 
-import com.project.schoolmanagment.entity.concretes.user.User;
-import com.project.schoolmanagment.payload.mappers.UserMapper;
+
 import com.project.schoolmanagment.payload.request.user.TeacherRequest;
 import com.project.schoolmanagment.payload.response.businnes.ResponseMessage;
-import com.project.schoolmanagment.payload.response.user.TeacherResponse;
 import com.project.schoolmanagment.payload.response.user.UserResponse;
-import com.project.schoolmanagment.repository.user.UserRepository;
-import com.project.schoolmanagment.service.buisnes.LessonProgramService;
-import com.project.schoolmanagment.service.helper.MetodHelper;
 import com.project.schoolmanagment.service.user.TeacherService;
-import com.project.schoolmanagment.service.user.UserRoleService;
-import com.project.schoolmanagment.service.validator.DateTimeValidator;
-import com.project.schoolmanagment.service.validator.UniquePropertyValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
